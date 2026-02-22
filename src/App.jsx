7d7da3966b25f2a492129f2fc4ac698c06e20b3a
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2026-02-22 22:46:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-return (
-  <div style={{ padding: 40 }}>
-    APP IS WORKING
-  </div>
-);
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      APP IS WORKING
+    </div>
+  );
+}
+
+export default App;
