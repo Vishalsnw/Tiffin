60b42c0d0babc03ec7d8644b840354a522eb65bd
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2026-02-22 22:49:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<div>Router Working</div>} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </HashRouter>
   );
